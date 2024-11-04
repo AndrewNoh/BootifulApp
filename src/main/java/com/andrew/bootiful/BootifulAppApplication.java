b1b11ea11a4390385225d30cc274b31ce7e86897
AndrewNoh <32383693+AndrewNoh@users.noreply.github.com>
2024-11-04 14:04:40 +0900

    Initial commit: Initialized Spring Boot application with base configuration

---
+++ src/main/java/com/andrew/bootiful/BootifulAppApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.bootiful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootifulAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrew/bootiful/BootifulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.bootiful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
